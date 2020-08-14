868e277279ff4ac374032d1298480c0c5ffa6438
a1chal <69692103+a1chal@users.noreply.github.com>
2020-08-14 11:01:50 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+    	System.out.println("My name is Achal Shah");
+		System.out.println("My hobbies are watching football and playing video games");
+		System.out.println("One of my favorite video games is Call of Duty");
+		System.out.println("When I go to college I want to major in either business or economics");
+		System.out.println("My dream college as of right now is NYU");
+		
+
+
+
+
+
+	}
+}
